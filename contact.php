c7d11f6323e38e5836e5737a772e5a2046ba61e6
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2017-05-30 16:51:40 -0400

    contact-form

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php 
 	if($_POST) {
 
-		$to = "jpunzone@gmail.com"; // Your email here
+		$to = "jennine@optonline.net"; // Your email here
 		$subject = 'Message from my website'; // Subject message here
 
 	}
