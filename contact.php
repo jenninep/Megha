1a9186898f29cfe734eb47020cd79cc4ba8b6c0d
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2017-06-22 08:09:10 -0400

    contact-form

--- contact.php
+++ contact.php
@@ -70,12 +70,13 @@ $result = $mgClient->sendMessage($domain, array(
     'to'      => 'Jennine Punzone <jennine@optonline.net>', 
     'subject' => $subject, 
     'text'    => $comment 
-)); 
-if($result) {
+    if($result) {
     echo json_encode(array('info' => 'success', 'msg' => "Your message has been sent. Thank you!"));
 } else {
     echo json_encode(array('info' => 'error', 'msg' => "Error, your message hasn't been sent")); 
 }
+)); 
+
 
 
  echo json_encode(array('info' => 'error', 'msg' => $result)); 
