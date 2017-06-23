c0df45c65ebe3474b893ec7f9aa21ab4b91207b0
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2017-06-23 14:29:48 -0400

    contact-form

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <?php  
  
-# Include the Autoloader (see "Libraries" for install instructions) 
-require 'vendor/autoload.php'; 
-use Mailgun\Mailgun; 
+// # Include the Autoloader (see "Libraries" for install instructions) 
+// require 'vendor/autoload.php'; 
+// use Mailgun\Mailgun; 
  
-# Instantiate the client. 
-$mgClient = new Mailgun('key-eea697e1dcd652c3c0386e8631785d13'); 
-$domain = "app5cc9dd4ad423413580c696abc5ae13ad.mailgun.org"; 
+// # Instantiate the client. 
+// $mgClient = new Mailgun('key-eea697e1dcd652c3c0386e8631785d13'); 
+// $domain = "app5cc9dd4ad423413580c696abc5ae13ad.mailgun.org"; 
 
 
 
@@ -64,25 +64,18 @@ $domain = "app5cc9dd4ad423413580c696abc5ae13ad.mailgun.org";
     } 
        
  
-# Make the call to the client. 
-$result = $mgClient->sendMessage($domain, array( 
-    'from'    => $mail, 
-    'to'      => 'Jennine Punzone <jennine@optonline.net>', 
-    'subject' => $subject, 
-    'text'    => $comment 
-)); 
-if($result) {
-    echo json_encode(array('info' => 'success', 'msg' => "Your message has been sent. Thank you!"));
-    
-} else {
-    echo json_encode(array('info' => 'error', 'msg' => "Error, your message hasn't been sent")); 
-}
+// # Make the call to the client. 
+// $result = $mgClient->sendMessage($domain, array( 
+//     'from'    => $mail, 
+//     'to'      => 'Jennine Punzone <jennine@optonline.net>', 
+//     'subject' => $subject, 
+//     'text'    => $comment 
+// )); 
 
 
 
  echo json_encode(array('info' => 'error', 'msg' => $result)); 
  
     send_mail($to, $subject, $comment . "\r\n\n"  .'Name: '.$name. "\r\n" .'Email: '.$mail, $headers);
-
  
 ?>
